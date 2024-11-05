c3a6431636a07bf1dc37dcd4c5745d8d49487e4a
ggang89 <ggang89@gmail.com>
2024-11-06 03:19:16 +0900

    동물마다 html,js파일 만들어서 연결해줌 => 1차 완성

--- src/koala.js
+++ src/koala.js
@@ -2,8 +2,8 @@ const API_URL = "https://animal-api-two.vercel.app/";
 
 let $content = document.querySelector("div.content");
 let template = [];
-const getData = async () => {
-  let res = await fetch(API_URL);
+const getData = async (name) => {
+  let res = await fetch(`${API_URL}${name}`);
   try {
     if (res) {
       let data = await res.json();
@@ -11,12 +11,10 @@ const getData = async () => {
         template += `<img src=${elm.url}/>`;
       });
       $content.innerHTML = template;
-      //innerHtml : 이전 요소의 자손이 모두 제거되고 지정된 요소로 대체
-      //img src가 content의 속성이 됨
     }
   } catch (error) {
     console.log(err);
   }
 };
 
-getData();
+getData("koala");
