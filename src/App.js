6c6e930468a57ce2bad43fbbc4087469f97c6d7b
ggang89 <ggang89@gmail.com>
2024-11-08 18:09:09 +0900

    APP컴포넌트에 state로 값 설정하고, 초기값과 탭 눌렀을 떄 변경되는 값 함수로 만들어서 전달

--- src/App.js
+++ src/App.js
@@ -1,9 +1,48 @@
-
 import Content from "./components/Content";
 import TabBar from "./components/TabBar";
+import { request } from "./components/api";
 
-export default function App($app) {//index.js에서 전달한 요소를 받음
-  
-  const tabBar = new TabBar();
+export default function App($app) {
+  //index.js에서 전달한 요소를 받음
+
+  // 필요한 상태 정의, 초기화
+  this.state = {
+    currentTab: "all",
+    photos: [],
+  };
+
+  const tabBar = new TabBar({
+    initialState: "",
+    onClick: async (name) => {
+      this.setState({
+        ...this.state,
+        currentTab: name,
+        photos: await request(name),
+      });
+    },
+  });
   const content = new Content();
+
+  // 상태 업데이트 함수
+  this.setState = (newState) => {
+    //업데이트할 새로운 값을 newState로 받아서 내부에 할당
+    this.state = newState;
+    tabBar.setState(this.state.currentTab);
+    content.setState(this.state.photos);
+  };
+
+  // 시작할 때 필요한 작업을 실행하고 초기상태를 설정하는 함수
+  const init = async () => {
+    try {
+      // 초기상태에 필요한 값 : 모든 동물의 사진
+      const initialPhotos = await request();
+      this.setState({
+        ...this.state,
+        photos: initialPhotos,
+      });
+    } catch (error) {
+      console.log(error);
+    }
+  };
+  init();
 }
